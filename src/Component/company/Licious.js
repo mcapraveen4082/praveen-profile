d18eca45f21dd5426a8ce477d644d1270c897066
praveen-vyapar <praveen@vyaparapp.in>
2024-03-15 18:14:27 +0530

    added files

--- src/Component/company/Licious.js
+++ src/Component/company/Licious.js
@@ -10,14 +10,26 @@ class Licious extends Component{
     render(){
         return (
             <div class="card">
-                <h2>Company Name</h2>
-                <p><strong>Industry:</strong> Industry Name</p>
-                <p><strong>Technologies Used:</strong> React.js, Nest.js, AWS (EC2, RDS, S3, IAM, CloudWatch)</p>
+                <h2><strong>Industry: </strong> Licious</h2>
+                <p><strong>Technologies Used:</strong> React.js, Javascript, Laravel, HTML, CSS</p>
                 <p><strong>Features Developed:</strong></p>
                 <ul>
-                    <li>Feature 1</li>
-                    <li>Feature 2</li>
-                    <li>Feature 3</li>
+                    <li>
+                        <b>Product Listing Page: </b>  
+                        <p>The product listing page should display detailed information about the product. Users should be able to view the product name, price, description, and other relevant details.</p>
+                    </li>
+                    <li>
+                        <b>Product Checkout Page: </b> 
+                        <p>The checkout page should allow users to review their selected products, enter their shipping and payment information, and complete the purchase process.</p> 
+                    </li>
+                    <li>
+                        <b>Paytm Payment Gateway: </b>  
+                        <p>Integrate the Paytm payment gateway into the application to allow users to make payments using the Paytm platform. Use the Paytm provided SDK or API to generate the payment request and initiate the payment flow.</p>
+                    </li>
+                    <li>
+                        <b>Coupons & Referral: </b>  
+                        <p>Implement a system for users to redeem coupons and refer friends to the platform, rewarding both the referrer and the referee. Allow admins to generate unique coupon codes with specific discounts or benefits</p>
+                    </li>
                 </ul>
             </div>
         );
--- src/Component/company/Vyapar.js
+++ src/Component/company/Vyapar.js
@@ -10,14 +10,21 @@ class Vyapar extends Component{
     render(){
         return (
             <div class="card">
-                <h2>Company Name</h2>
-                <p><strong>Industry:</strong> Industry Name</p>
-                <p><strong>Technologies Used:</strong> React.js, Nest.js, AWS (EC2, RDS, S3, IAM, CloudWatch)</p>
+                <h2><strong>Industry: </strong> Vyapar App</h2>
+                <p><strong>Technologies Used:</strong> React.js, Nest.js, Next.js, Laravel, HTML, CSS</p>
                 <p><strong>Features Developed:</strong></p>
                 <ul>
-                    <li>Feature 1</li>
-                    <li>Feature 2</li>
-                    <li>Feature 3</li>
+                     <li>
+                        <b>Calling Support Internal Dashboard: </b>  
+                        <p>Integrate calling support into the dashboard to allow agents to make calls directly from the dashboard. Additionally, automate lead assignment based on predefined rules. Log call details (such as call duration, timestamp, and caller ID) in the dashboard for future reference. Provide users with access to their call history, including details of past calls made from the dashboard</p>
+                        <p>Define rules for automatically assigning leads to agents based on criteria such as lead source, location, or lead score.</p>
+                    </li>
+                    <li>
+                        <b>Distributor Commission: </b>  
+                        <p>Automate the process of calculating distributor commissions based on sales data and facilitate payment to distributors. Provide a reporting mechanism to view commission details and statements.</p>
+                        <p>Define a payment schedule (e.g., weekly, monthly) for distributing commissions to distributors</p>
+                        <p></p>
+                    </li>
                 </ul>
             </div>
         );
