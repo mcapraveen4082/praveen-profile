35fb2e1423115a075b40d51deca2c809816e0a90
praveen-vyapar <praveen@vyaparapp.in>
2026-06-11 15:13:12 +0530

    added

--- src/Component/snippets/Footer.tsx
+++ src/Component/snippets/Footer.tsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import { Nav } from 'react-bootstrap';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Nav, Row, Col } from 'react-bootstrap';
 
 class Footer extends Component {
   render() {
