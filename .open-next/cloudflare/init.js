28c3345dfb42a751d3cce61a4ee47a441954c652
praveen-vyapar <praveen@vyaparapp.in>
2026-06-11 18:55:09 +0530

    added fix

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1781179857502,
+    __BUILD_TIMESTAMP_MS__: 1781184269376,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: true,
